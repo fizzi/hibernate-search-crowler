873bd4479da1bf3420ef9cbcd7ca2f4272fbcc1d
Francesco Izzi <francesco.izzi@gmail.com>
2013-01-09 12:42:34 +0100

    add natural search phrase test

--- src/test/java/org/geosdi/IndexAndSearchTest.java
+++ src/test/java/org/geosdi/IndexAndSearchTest.java
@@ -13,19 +13,18 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
+
 import org.apache.lucene.util.Version;
+import org.hibernate.search.SearchFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.hibernate.Session;
-import org.hibernate.search.FullTextSession;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
-
-import static org.junit.Assert.assertEquals;
+import org.hibernate.search.query.dsl.QueryBuilder;
 
 /**
  * Example testcase for Hibernate Search
@@ -46,29 +45,35 @@ public class IndexAndSearchTest {
         //purge();
     }
 
+//    @Test
+//    public void testIndexAndSearch() throws Exception {
+//        List<Contact> contacts = new ArrayList<Contact>();
+//
+//
+//        index();
+//        double startTime = System.currentTimeMillis();
+//        contacts = search("regione calabria assessore");
+//        long endTime = System.currentTimeMillis();
+//        log.info("Tempo = " + (endTime - startTime) / 1000);
+//
+//        log.info("RISULTATI LETTI DALL'indice: " + contacts.size());
+//
+//
+//    }
     @Test
-    public void testIndexAndSearch() throws Exception {
+    public void testStemming() throws Exception {
         List<Contact> contacts = new ArrayList<Contact>();
-
-        
         index();
-        double startTime = System.currentTimeMillis(); 
-        contacts = search("assessorato");
-        long endTime = System.currentTimeMillis(); 
-        log.info("Tempo = " + (endTime - startTime)/1000);
-        
+        double startTime = System.currentTimeMillis();
+        contacts = searchPhrase("regione piemonte");
+        long endTime = System.currentTimeMillis();
+        log.info("Tempo = " + (endTime - startTime) / 1000);
+
         log.info("RISULTATI LETTI DALL'indice: " + contacts.size());
 
 
     }
 
-//	@Test
-//	public void testStemming() throws Exception {
-//
-//		//index();
-//
-//		
-//	}
     private void initHibernate() {
         emf = Persistence.createEntityManagerFactory("hibernate-search-example");
         em = emf.createEntityManager();
@@ -102,6 +107,17 @@ public class IndexAndSearchTest {
         return contacts;
     }
 
+    private List<Contact> searchPhrase(String searchQuery) throws ParseException {
+        Query query = searchQueryPhrase(searchQuery);
+
+        List<Contact> contacts = query.getResultList();
+
+        for (Contact c : contacts) {
+            log.info("Business Name: " + c.getBusinessName() + " PHONE/FAX: " + c.getPhoneNumbers());
+        }
+        return contacts;
+    }
+
     private Query searchQuery(String searchQuery) throws ParseException {
 
         String[] contactFields = {"businessName", "category"};
@@ -123,5 +139,24 @@ public class IndexAndSearchTest {
         final FullTextQuery query = ftEm.createFullTextQuery(luceneQuery, Contact.class);
 
         return query;
+
+    }
+
+    private Query searchQueryPhrase(String phrase) {
+        FullTextEntityManager fullTextSession = org.hibernate.search.jpa.Search.getFullTextEntityManager(em);
+        SearchFactory searchFactory = fullTextSession.getSearchFactory();
+
+        QueryBuilder mythQB = searchFactory.buildQueryBuilder().forEntity(Contact.class).get();
+
+        org.apache.lucene.search.Query luceneQuery = mythQB
+                .phrase()
+                .withSlop(3)
+                .onField("businessName")
+                .sentence(phrase)
+                .createQuery();
+
+        final FullTextQuery query = fullTextSession.createFullTextQuery(luceneQuery, Contact.class);
+        return query;
+
     }
 }
